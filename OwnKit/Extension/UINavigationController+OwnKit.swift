18e3e056e9a66508f271d7afaa2cf1762bcf5f39
Ryo Aoyama <r.fe51028.r@gmail.com>
2016-04-14 13:16:30 +0900

    updated sig name

--- OwnKit/Extension/UINavigationController+OwnKit.swift
+++ OwnKit/Extension/UINavigationController+OwnKit.swift
@@ -2,7 +2,7 @@
 //  UINavigationController.swift
 //  OwnKit
 //
-//  Created by 青山 遼 on 2016/04/14.
+//  Created by Ryo Aoyama on 2016/04/14.
 //  Copyright © 2016年 Ryo Aoyama. All rights reserved.
 //
 
