4a33b01988551a859199b26c0b9832a2206784ef
Ryo Aoyama <r.fe51028.r@gmail.com>
2016-04-23 00:14:29 +0900

    bump up version to 0.2.8

--- OwnKit/Extension/UIImage+OwnKit.swift
+++ OwnKit/Extension/UIImage+OwnKit.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Accelerate
 
 public extension UIImage {
     func roundedImage(radius: CGFloat, size: CGSize? = nil) -> UIImage {
@@ -19,6 +20,67 @@ public extension UIImage {
         let roundedImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         return roundedImage
+    }
+    
+    func blurredImage(radius: CGFloat, iterations: Int = 1, ratio: CGFloat = 1, blendColor: UIColor? = nil) -> UIImage {
+        if floorf(Float(size.width)) * floorf(Float(size.height)) <= 0.0 || radius <= 0 {
+            return self
+        }
+        
+        let imageRef = CGImage
+        var boxSize = UInt32(radius * scale * ratio)
+        if boxSize % 2 == 0 { boxSize += 1 }
+        
+        let height = CGImageGetHeight(imageRef)
+        let width = CGImageGetWidth(imageRef)
+        let rowBytes = CGImageGetBytesPerRow(imageRef)
+        let bytes = rowBytes * height
+        
+        let inData = malloc(bytes)
+        var inBuffer = vImage_Buffer(data: inData, height: UInt(height), width: UInt(width), rowBytes: rowBytes)
+        
+        let outData = malloc(bytes)
+        var outBuffer = vImage_Buffer(data: outData, height: UInt(height), width: UInt(width), rowBytes: rowBytes)
+        
+        let tempFlags = vImage_Flags(kvImageEdgeExtend + kvImageGetTempBufferSize)
+        let tempSize = vImageBoxConvolve_ARGB8888(&inBuffer, &outBuffer, nil, 0, 0, boxSize, boxSize, nil, tempFlags)
+        let tempBuffer = malloc(tempSize)
+        
+        let provider = CGImageGetDataProvider(imageRef)
+        let copy = CGDataProviderCopyData(provider)
+        let source = CFDataGetBytePtr(copy)
+        memcpy(inBuffer.data, source, bytes)
+        
+        let flags = vImage_Flags(kvImageEdgeExtend)
+        
+        iterations.times { _ in
+            vImageBoxConvolve_ARGB8888(&inBuffer, &outBuffer, tempBuffer, 0, 0, boxSize, boxSize, nil, flags)
+            
+            let temp = inBuffer.data
+            inBuffer.data = outBuffer.data
+            outBuffer.data = temp
+        }
+        
+        let colorSpace = CGImageGetColorSpace(imageRef)
+        let bitmapInfo = CGImageGetBitmapInfo(imageRef)
+        let bitmapContext = CGBitmapContextCreate(inBuffer.data, width, height, 8, rowBytes, colorSpace, bitmapInfo.rawValue)
+        defer {
+            free(outBuffer.data)
+            free(tempBuffer)
+            free(inBuffer.data)
+        }
+        
+        if let color = blendColor {
+            CGContextSetFillColorWithColor(bitmapContext, color.CGColor)
+            CGContextSetBlendMode(bitmapContext, CGBlendMode.PlusLighter)
+            CGContextFillRect(bitmapContext, CGRect(x: 0, y: 0, width: width, height: height))
+        }
+        
+        if let bitmap = CGBitmapContextCreateImage(bitmapContext) {
+            return UIImage(CGImage: bitmap, scale: scale, orientation: imageOrientation)
+        }
+        
+        fatalError()
     }
     
     static func colorImage(
